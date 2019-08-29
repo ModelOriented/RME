29bd9c220cc7af2a938ddfbbc64c6bbf23885ab2
Mateusz Kobyłka <32520662+kobylkam@users.noreply.github.com>
2019-08-29 10:20:37 +0200

    Added new tests for explainer

--- RME/tests/test_explainer.py
+++ RME/tests/test_explainer.py
@@ -42,4 +42,23 @@ def test_instance_explanation(get_data, classifier):
     with pytest.raises(Exception, match=r"distance should be either  'L1' or 'L2'. The value of type was: L3"):
         explainer.explain_instance(instance=['To be explained'], predict_function=classifier, distance='L3',
                                    class_index=0)
-        
+
+    explainer.explain_instance(instance=['To be explained'], predict_function=classifier, distance='L1', class_index=0)
+
+    assert len(explainer.time_steps) == 3
+    assert explainer.perturbed_probabilities.shape == (len(explainer.vocabulary), 3)
+    assert explainer.probability_change.shape == (len(explainer.vocabulary), 3)
+    assert len(explainer.mean_absolute) == 3
+    assert len(explainer.mean_change) == 3
+    assert len(explainer.variances) == 3
+    assert explainer.instance_value == 0.5
+
+
+def test_step_by_step_prediction(get_data, classifier):
+
+    explainer = Explainer(get_data.loc[:, 'Sentence'], vocabulary=['this', 'is', 'positive', 'negative',
+                                                                   'sentence', 'review', 'comments'])
+    explainer.step_by_step_prediction(instance = ['Getting positive'], predict_function=classifier)
+
+    assert all([obtained == expected for obtained, expected
+                in zip(explainer.partial_predictions, [0, 1])])
